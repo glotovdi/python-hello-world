9c8fabc27b29193b8cd0eea7001acd12002c660e
Глотов Дмитрий <glotovdi@gmail.com>
2019-06-05 16:35:49 +0300

    no message

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import requests
+from requests.exceptions import HTTPError
+
 def greetings(name):
     print(f'Здарова, {name}!')
 
@@ -6,4 +9,17 @@ def main():
     greetings(name)
 
 
-main()
+def request():
+    try:
+        response = requests.get('https://api.github.com')
+        response.raise_for_status()
+    except HTTPError as http_err:
+        print(f'HTTP error occurred: {http_err}')
+    except Exception as err:
+        print(f'Other error occurred: {err}')
+    else:
+        print('Success!')
+
+request()
+
+
